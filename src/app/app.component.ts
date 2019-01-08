5eccac7f368067a3986b04d12fc5325f55fe7be8
MartinLambert <martin.lambert@verizon.net>
2019-01-08 10:48:40 -0500

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'hotm-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'MountainWeb';
+}
